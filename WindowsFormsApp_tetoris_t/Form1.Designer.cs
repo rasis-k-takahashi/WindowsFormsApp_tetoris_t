8177d190dfc5d2e7cc78fba610a6e418092ec761
rasis-k-takahashi <takahashi@rasis-soft.co.jp>
2021-03-03 16:40:36 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp_tetoris_t/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace WindowsFormsApp_tetoris_t
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(20, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 281);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(251, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 281);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp_tetoris_t/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_tetoris_t
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ペイント　イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // 画像読み込み
+            Image image = Image.FromFile(@"D:\01_sorce\test\WindowsFormsApp_tetoris_t\WindowsFormsApp_tetoris_t\Tetris.bmp");
+
+            // 切り取り
+            const int length = 20;
+            const int nColor = 8;
+            //1 :::var srcRect = new Rectangle(3 * length, 0, length, length);
+            var srcRect = new Rectangle(3 * length, 0, length, length);
+            var destRect = new Rectangle(length, length, length, length);
+            e.Graphics.DrawImage(image, destRect, srcRect, GraphicsUnit.Pixel);
+
+
+
+            //var srcRect = new Rectangle[nColor];
+            //for (int i = 0; i > nColor; i++)
+            //{
+            //    srcRect[i] = new Rectangle(i * length, 0, length, length);
+            //}
+
+            //// 切り取った絵を表示する位置と大きさを決める領域
+            //const int width = 10;
+            //const int height = 20;
+            //var destRect = new Rectangle[width, height];
+            //for (int i = 0; i < width; i++)
+            //{
+            //    for (int j = 0; j < height; j++)
+            //    {
+            //        destRect[i, j] = new Rectangle(i * length, j * length, length, length);
+            //    }
+            //}
+
+            //// 描画
+            //e.Graphics.DrawImage(image, destRect[1,1], srcRect[4], GraphicsUnit.Pixel);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // 画像読み込み
+            Image image = Image.FromFile(@"D:\01_sorce\test\WindowsFormsApp_tetoris_t\WindowsFormsApp_tetoris_t\Tetris.bmp");
+
+            // 切り取り
+            const int length = 20;
+            const int nColor = 8;
+            //1 :::var srcRect = new Rectangle(3 * length, 0, length, length);
+            var srcRect = new Rectangle(3 * length, 0, length, length);
+            var destRect = new Rectangle[10, 20];
+            e.Graphics.DrawImage(image, destRect[1, 1], srcRect, GraphicsUnit.Pixel);
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // 画像読み込み
+            Image image = Image.FromFile("Tetris.bmp");
+
+            // 切り取り
+            const int length = 20;
+            const int nColor = 8;
+            //1 :::var srcRect = new Rectangle(3 * length, 0, length, length);
+            var srcRect = new Rectangle(4 * length, 0, length, length);
+            var destRect = new Rectangle(length, length, length, length);
+            e.Graphics.DrawImage(image, destRect, srcRect, GraphicsUnit.Pixel);
+
+        }
+    }
+}
