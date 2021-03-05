b67f730c57c645c973b15178c6628130466b3ca6
rasis-k-takahashi <takahashi@rasis-soft.co.jp>
2021-03-05 11:04:58 +0900

    リファクタリング

--- WindowsFormsApp_tetoris_t/Form1.Designer.cs
+++ WindowsFormsApp_tetoris_t/Form1.Designer.cs
@@ -62,6 +62,7 @@ namespace WindowsFormsApp_tetoris_t
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
